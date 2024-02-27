3e38d63ab806dfa81bb6c0a87340cc3c914c6ba4
邱万烽 <qwf18650819181@163.com>
2024-02-27 17:23:43 +0800

    comments

--- src/main/java/com/comeup/slflog/Main.java
+++ src/main/java/com/comeup/slflog/Main.java
@@ -1,16 +1,21 @@
 package com.comeup.slflog;
 
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.slf4j.Marker;
 import org.slf4j.MarkerFactory;
 
+import java.time.LocalDateTime;
+
 /**
  * @auth: qwf
  * @date: 2024年1月22日 0022
- * @description:
+ * @description: 日志 打多打少都不好,恰如其分最重要
  */
+@Slf4j
 public class Main {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
@@ -25,9 +30,23 @@ public class Main {
         MDC.put("discriminator", "STDOUT2");
         LOGGER.warn("LOGGER");
         LOGGER.info("logger");
+        LOGGER.info("{}", LocalDateTime.now());
 
         SpecificLogger specificLogger = new SpecificLogger();
         specificLogger.testLog();
+
+        log.info("[参数] Main main args: {} timestamp: {}", JSON.toJSONString(args), LocalDateTime.now());
+
+//        log.info("[$TYPE$] $CLASS$ $METHOD$ $OBJECT$: {} timestamp: {}", JSON.toJSONString($OBJECT$), LocalDateTime.now());
+
+        try {
+
+        } catch (Exception e) {
+            log.error("失败错误异常", e);
+        }
+        log.warn("[参数] Main main request: {} timestamp: {}", JSON.toJSONString(specificLogger), LocalDateTime.now());
+
+
     }
 
 
