c93c775dd968c356a571463b242ac9fe770caafe
邱万烽 <qwf18650819181@163.com>
2024-04-02 18:00:36 +0800

    LeetCode560

---
+++ src/main/java/com/comeup/leetcode/LeetCode560.java
@@ -0,0 +1,66 @@
+package com.comeup.leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * 功能描述: 560. 和为 K 的子数组
+ * 给你一个整数数组 nums 和一个整数 k ，请你统计并返回 该数组中和为 k 的子数组的个数 。
+ *
+ * 子数组是数组中元素的连续非空序列。
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1,1,1], k = 2
+ * 输出：2
+ * 示例 2：
+ *
+ * 输入：nums = [1,2,3], k = 3
+ * 输出：2
+ *
+ * note: 前缀和
+ * @author qiu wanzi
+ * @date 2024年3月12日 0012
+ */
+public class LeetCode560 {
+
+    public int subarraySum(int[] nums, int k) {
+        if (nums == null || nums.length == 0) return 0;
+        Map<Integer, Integer> preMap = new HashMap<>();
+        preMap.put(0, 1);
+        int pre = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            pre += nums[i];
+            if (preMap.containsKey(pre - k)) {
+                count += preMap.get(pre - k);
+            }
+            preMap.merge(pre, 1, Integer::sum);
+        }
+        return count;
+    }
+
+
+    /**
+     * 超时
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int subarraySum1(int[] nums, int k) {
+        List<Integer> list = Arrays.stream(nums).boxed().collect(Collectors.toList());
+        int count =  0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = nums.length; j > i; j--) {
+                int sum = list.subList(i, j).stream().mapToInt(Integer::intValue).sum();
+                if (sum == k) count++;
+            }
+        }
+        return count;
+    }
+
+
+}
