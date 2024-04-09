6ddf6f501e54dcb250b0f9fbad20ee76b1d7ce4c
邱万烽 <qwf18650819181@163.com>
2024-04-09 17:00:03 +0800

    LeetCode189

---
+++ src/main/java/com/comeup/algorithm/leetcode/LeetCode189.java
@@ -0,0 +1,40 @@
+package com.comeup.algorithm.leetcode;
+
+/**
+ * 189. 轮转数组
+ * 给定一个整数数组 nums，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
+ *
+ * 示例 1:
+ *
+ * 输入: nums = [1,2,3,4,5,6,7], k = 3
+ * 输出: [5,6,7,1,2,3,4]
+ * 解释:
+ * 向右轮转 1 步: [7,1,2,3,4,5,6]
+ * 向右轮转 2 步: [6,7,1,2,3,4,5]
+ * 向右轮转 3 步: [5,6,7,1,2,3,4]
+ * 示例 2:
+ *
+ * 输入：nums = [-1,-100,3,99], k = 2
+ * 输出：[3,99,-1,-100]
+ * 解释:
+ * 向右轮转 1 步: [99,-1,-100,3]
+ * 向右轮转 2 步: [3,99,-1,-100]
+ */
+public class LeetCode189 {
+
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        int index = nums.length - k;
+
+        int[] helper = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            helper[i] = nums[i];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = helper[index++ % nums.length];
+        }
+    }
+
+
+}
